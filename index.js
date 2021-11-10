e2a11c36d9da1234da872198db1285c6264dd880
Sam Parsons <samparsons269@gmail.com>
2021-11-09 23:44:30 -0500

    update index

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const pg = require('pg');
 const express = require('express');
-
+const { Pool } = require('pg');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {res.send("successly")});
+const pool = new Pool({ // create connection to database
+  connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+  ssl: {
+    rejectUnauthorized: false // don't check for SSL cert
+  }
+});
+
+app.get('/', (req, res) => {
+    pool.query("SELECT * FROM timesheets")
+        .then(result => res.json(result))
+        .catch(err => console.log(err));
+});
 
 app.listen(PORT, () => console.log("connected"));
