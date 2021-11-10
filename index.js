5c3c2e4e227a9a48b343f5caedba115929245549
Sam Parsons <samparsons269@gmail.com>
2021-11-09 23:33:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const pg = require('pg');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {res.send("success")});
+
+app.listen(PORT, () => console.log("connected"));
