00c1ed14cd9ac9f839d5f60200b89f21ddad601a
sam parsons <samparsons269@gmail.com>
2021-11-10 22:22:14 -0500

    added fundamental react files

---
+++ client/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import SummaryTable from './components/SummaryTable';
+import aggregate from './util/aggregate';
+import flatten from './util/flatten';
+import './app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      summaryData: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/timesheets/')
+      .then((res) => res.json())
+      .then((rows) => {
+        this.setState({ summaryData: flatten(aggregate(rows)) });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Hello World</div>
+        {this.state.summaryData ? (
+          <SummaryTable summaryData={this.state.summaryData} />
+        ) : null}
+      </div>
+    );
+  }
+}
