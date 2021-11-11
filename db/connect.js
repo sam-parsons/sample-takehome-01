b17c6143133aa402712ce3508a2c7c6305b3bb6d
sam parsons <samparsons269@gmail.com>
2021-11-11 00:00:19 -0500

    updated local connection string for db

--- db/connect.js
+++ db/connect.js
@@ -1,20 +1,9 @@
 const { Client } = require('pg');
 
 let config = {};
-
-if (process.env.DATABASE_URL) {
-  config.connectionString = process.env.DATABASE_URL;
-} else {
-  config = {
-    user: process.env.user,
-    host: 'localhost',
-    database: 'postgres',
-    password: process.env.password,
-    port: 5432,
-  };
-}
-
+config.connectionString = process.env.DATABASE_URL || url;
 const client = new Client(config);
+
 client
   .connect()
   .then(() => console.log('connected'))
