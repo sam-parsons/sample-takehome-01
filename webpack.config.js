71257bb55ea3a71352379c6c81aae047f2e656c5
sam parsons <samparsons269@gmail.com>
2021-11-10 22:18:04 -0500

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const outputDirectory = 'build';
+
+module.exports = {
+  entry: ['babel-polyfill', './client/index.jsx'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      // favicon
+    }),
+  ],
+};
