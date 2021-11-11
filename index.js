94928ca21129b787ad752f5682f347ca2eeb299c
sam parsons <samparsons269@gmail.com>
2021-11-10 23:05:49 -0500

    PORT logging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const pool = require('./db/connect');
+const { client: pool } = require('./db/connect');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const checkAndSeedDB = require('./db/seed');
@@ -22,4 +22,4 @@ app.get('/api/timesheets/:client', timesheetController.getOneEntry);
 
 app.post('/api/timesheets', timesheetController.createEntry);
 
-app.listen(PORT, () => console.log('connected'));
+app.listen(PORT, () => console.log(`PORT: ${PORT}`));
