10d99282828471baa80f6735e91876a24b437c58
sam parsons <samparsons269@gmail.com>
2021-11-11 00:11:29 -0500

    wrapped code in async

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const PORT = process.env.PORT || 5000;
 const checkAndSeedDB = require('./db/seed');
 const timesheetController = require('./server/controllers/timesheet');
 
-// json parsins and static files
-app.use(express.json());
-app.use(express.static('build'));
+const main = async function () {
+  // json parsins and static files
+  app.use(express.json());
+  app.use(express.static('build'));
 
-// seed table if doesn't exist
-(async function () {
+  // seed table if doesn't exist
   await checkAndSeedDB(pool);
-})();
 
-// Routes
-app.get('/api/timesheets', timesheetController.getAllEntries);
+  // Routes
+  app.get('/api/timesheets', timesheetController.getAllEntries);
 
-app.get('/api/timesheets/:client', timesheetController.getOneEntry);
+  app.get('/api/timesheets/:client', timesheetController.getOneEntry);
 
-app.post('/api/timesheets', timesheetController.createEntry);
+  app.post('/api/timesheets', timesheetController.createEntry);
 
-app.listen(PORT, () => console.log(`PORT: ${PORT}`));
+  app.listen(PORT, () => console.log(`PORT: ${PORT}`));
+};
+
+main().catch((err) => console.log(err));
